b3a12c3fb1270de5220fdff33f49e142874fae20
dc <chendana95@gmail.com>
2020-03-05 21:12:02 -0500

    created post route to add car obj to db

--- App/routes/api-routes.js
+++ App/routes/api-routes.js
@@ -1,2 +1,17 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
+var carInfo = require("../public/js/car.js")
+const vin = "5XXGN4A70CG022862";
+module.exports = (app) => {
+    app.post("/api/cars", function(req, res) {
+        // Add sequelize code for creating a post using req.body,
+        // then return the result using res.json
+
+        carInfo(vin).then(data => {
+            console.log(data)
+            db.Car.create(data).then(dbCar => res.json(dbCar));
+
+        });
+
+    });
+}
