9061f84abef7e7e0bce7b4ca3a09223adcfcbefb
jsmithxyz <jordansmith3813@gmail.com>
2020-03-09 12:56:53 -0400

    created Watchlist functionality for /Home, and updated HTML to suit functionality

---
+++ public/js/watchlist.js
@@ -0,0 +1,14 @@
+// $(document).ready(() => {
+
+// const addWatchBtn = document.querySelector(".add-to-favs");
+
+// addWatchBtn.addEventListener("click", function(event) {
+    
+//     event.preventDefault();
+//     console.log("hit");
+//     const savedCar = infoArr;
+    
+//     localStorage.setItem("savedCar", JSON.stringify(savedCar));
+    
+//   });
+// })
