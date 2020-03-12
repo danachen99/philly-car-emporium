6f90cdc96a089a2aae050ecd4be44510fb37bdd2
dc <chendana95@gmail.com>
2020-03-12 02:24:43 -0400

    created model for image and created association

---
+++ models/image.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+    let Image = sequelize.define("Image", {
+        image: DataTypes.STRING
+    });
+    Image.associate = function(models) {
+        //Image belongs to Car
+        Image.belongsTo(models.Car)
+    }
+    return Image;
+}
